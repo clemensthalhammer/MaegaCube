0bd732b5f6f5de9de404948ad63ab06de1282223
clemens.thalhammer <clemens.thalhammer@AMS-GYM-EDV1-03.gym.ams.local>
2018-11-26 17:50:34 +0100

    26.11

--- MägaCube/src/maegacube_bewegungen/Maegacube_drehen.java
+++ MägaCube/src/maegacube_bewegungen/Maegacube_drehen.java
@@ -3,6 +3,7 @@
 package maegacube_bewegungen;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 
@@ -11,8 +12,6 @@ public class Maegacube_drehen {
 	public static void main(String[] args) {
 		
 		RegulatedMotor m = new EV3LargeRegulatedMotor(MotorPort.A);
-		
-		m.rotate(90);
 		
 
 		
