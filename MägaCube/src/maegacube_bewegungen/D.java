48fa35623090c518a72ab5458ac54bf75f28c0bf
clemens.thalhammer <clemens.thalhammer@AMS-GYM-EDV1-03.gym.ams.local>
2019-01-21 17:59:02 +0100

    1

--- MägaCube/src/maegacube_bewegungen/D.java
+++ MägaCube/src/maegacube_bewegungen/D.java
@@ -9,7 +9,6 @@ public class D {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[]
 		
 		//Maegacube_griffel.maegacube_halten();
 		///Maegacube_drehen.maegacube_drehen(1);
